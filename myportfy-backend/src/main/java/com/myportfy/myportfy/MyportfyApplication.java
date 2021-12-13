7ed79cfef577154bbcaa0a5a4d246b0ff7644737
dev-Pedrod <pedroolivxira@gmail.com>
2021-12-13 13:32:21 -0300

    project created

---
+++ myportfy-backend/src/main/java/com/myportfy/myportfy/MyportfyApplication.java
@@ -0,0 +1,13 @@
+package com.myportfy.myportfy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyportfyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyportfyApplication.class, args);
+	}
+
+}
---
+++ myportfy-backend/src/test/java/com/myportfy/myportfy/MyportfyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myportfy.myportfy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyportfyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
