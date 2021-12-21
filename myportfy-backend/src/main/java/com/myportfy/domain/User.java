fc4ffa45e95b800f3757c3a43b31b7bae45dd010
dev-Pedrod <pedroolivxira@gmail.com>
2021-12-21 20:52:04 -0300

    feat(User): User entity and Gender

---
+++ myportfy-backend/src/main/java/com/myportfy/domain/User.java
@@ -0,0 +1,43 @@
+package com.myportfy.domain;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.myportfy.domain.enums.Gender;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.Email;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@Where(clause = "deleted_at is null")
+public class User extends DomainEntity{
+
+    private String firstName;
+    private String lastName;
+    @JsonFormat(pattern="yyyy-MM-dd")
+    private Date birthDate;
+    private Integer gender;
+    @Column(unique = true)
+    @Email
+    private String email;
+    @OneToMany
+    private List<Post> posts = new ArrayList<>();
+
+    public User(String firstName, String lastName, Date birthDate, Gender gender, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+        this.gender = (gender == null) ? null : gender.getId();
+        this.email = email;
+    }
+}
