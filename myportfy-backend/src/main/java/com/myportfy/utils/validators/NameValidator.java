b750127fec003f5aed6feee97e10eb8e72bb0393
dev-Pedrod <pedroolivxira@gmail.com>
2021-12-28 14:35:09 -0300

    feat(utils): name validator

--- myportfy-backend/src/main/java/com/myportfy/utils/validators/NameValidator.java
+++ myportfy-backend/src/main/java/com/myportfy/utils/validators/NameValidator.java
@@ -5,8 +5,15 @@ import java.util.regex.Pattern;
 
 public class NameValidator {
 
-    public static Boolean validateUserName(String name) {
+    public static Boolean validateUsername(String userName) {
         Pattern pattern = Pattern.compile("^[a-zA-Z0-9]+([._-]?[a-zA-Z0-9]+)*$");
+        Matcher matcher = pattern.matcher(userName);
+
+        return matcher.find();
+    }
+
+    public static Boolean validateName(String name) {
+        Pattern pattern = Pattern.compile("^[a-zA-ZÀ-ú]+([ ']?[a-zA-ZÀ-ú])*$");
         Matcher matcher = pattern.matcher(name);
 
         return matcher.find();
