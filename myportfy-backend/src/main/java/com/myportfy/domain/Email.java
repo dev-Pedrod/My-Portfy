c92edf3651a98bf6781104acec710be44d0d3c11
dev-Pedrod <pedroolivxira@gmail.com>
2022-01-30 15:02:09 -0300

    feat(Email): Email entity

---
+++ myportfy-backend/src/main/java/com/myportfy/domain/Email.java
@@ -0,0 +1,23 @@
+package com.myportfy.domain;
+
+import com.myportfy.domain.enums.StatusEmail;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity(name = "_email")
+public class Email extends DomainEntity{
+
+    private String emailFrom;
+    private String emailTo;
+    private String subject;
+    @Column(columnDefinition = "TEXT")
+    private String content;
+    private StatusEmail statusEmail;
+}
