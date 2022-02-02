5f2ea5fe020e35b948f9db80f3f3aa478c008d59
dev-Pedrod <pedroolivxira@gmail.com>
2022-02-02 10:46:51 -0300

    feat(Confirm-account): Token Confirmation Service

---
+++ myportfy-backend/src/main/java/com/myportfy/services/IConfirmationTokenService.java
@@ -0,0 +1,7 @@
+package com.myportfy.services;
+
+import com.myportfy.domain.ConfirmationToken;
+
+public interface IConfirmationTokenService extends HelperService<ConfirmationToken>{
+    ConfirmationToken findByToken(String token);
+}
