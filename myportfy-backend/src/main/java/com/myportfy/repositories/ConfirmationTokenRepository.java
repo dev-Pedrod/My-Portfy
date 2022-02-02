0af4f773dbfbdc0b92d0d794dae299e30aa58d62
dev-Pedrod <pedroolivxira@gmail.com>
2022-02-02 10:43:22 -0300

    feat(Confirm-account): Confirmation Token Repository

---
+++ myportfy-backend/src/main/java/com/myportfy/repositories/ConfirmationTokenRepository.java
@@ -0,0 +1,12 @@
+package com.myportfy.repositories;
+
+import com.myportfy.domain.ConfirmationToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ConfirmationTokenRepository extends JpaRepository<ConfirmationToken, Long> {
+    Optional<ConfirmationToken> findByToken(String token);
+}
