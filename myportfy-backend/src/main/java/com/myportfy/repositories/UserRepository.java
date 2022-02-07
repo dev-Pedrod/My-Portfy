42de9814f4a8df04d26910beda6892e5c4f6d6f8
dev-Pedrod <pedroolivxira@gmail.com>
2022-02-07 11:52:28 -0300

    feat(Confirm-account): Query to enable user

--- myportfy-backend/src/main/java/com/myportfy/repositories/UserRepository.java
+++ myportfy-backend/src/main/java/com/myportfy/repositories/UserRepository.java
@@ -2,7 +2,10 @@ package com.myportfy.repositories;
 
 import com.myportfy.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -12,4 +15,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
     User findByUsernameIgnoreCase(String username);
     List<User> findByUsernameStartsWithIgnoreCase(String username);
+    @Transactional
+    @Modifying
+    @Query("UPDATE _user x SET x.enabled = TRUE WHERE x.id = ?1")
+    void enableUser(Long id);
 }
