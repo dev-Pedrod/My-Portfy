8e95a703d9243bd8d7079e9d3f415f7475a6c645
dev-Pedrod <pedroolivxira@gmail.com>
2022-02-22 11:59:08 -0300

    refactor(forgotPassword): changing http method to POST

--- myportfy-backend/src/main/java/com/myportfy/config/SecurityConfig.java
+++ myportfy-backend/src/main/java/com/myportfy/config/SecurityConfig.java
@@ -42,12 +42,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private static final String[] PUBLIC_MATCHERS_GET = {
             "/categories/**",
             "/posts/**",
-            "/users/confirm-account",
-            "/auth/forgot-password",
+            "/users/confirm-account"
     };
 
     private static final String[] PUBLIC_MATCHERS_POST = {
-            "/users"
+            "/users",
+            "/auth/forgot-password",
     };
 
     private static final String[] PUBLIC_MATCHERS_PUT = {
