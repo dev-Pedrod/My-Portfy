3bb81589b42bfacee20f9d62c3b4fac65109158d
dev-Pedrod <pedroolivxira@gmail.com>
2022-03-08 10:19:55 -0300

    PostGetDto and UserGetDto

---
+++ myportfy-backend/src/main/java/com/myportfy/dto/user/UserGetDto.java
@@ -0,0 +1,17 @@
+package com.myportfy.dto.user;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.myportfy.dto.DtoDomain;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
+
+@Getter @Setter
+@NoArgsConstructor
+@JsonInclude(NON_NULL)
+public class UserGetDto extends DtoDomain {
+    private String username;
+    private String fullName;
+}
