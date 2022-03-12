87a1729ef072a54eecdf55aa4c46fe3adf74f0dd
dev-Pedrod <pedroolivxira@gmail.com>
2022-03-12 13:46:08 -0300

    Configuring styled-components

--- myportfy-frontend/src/index.jsx
+++ myportfy-frontend/src/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import { GlobalStyles } from './styles/global-styles';
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyles/>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
---
+++ myportfy-frontend/src/styles/global-styles.js
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+
+    * {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
+    
+`
