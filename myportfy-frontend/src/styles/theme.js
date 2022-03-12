2ef9d5d4042b54887d1a6afdbe32e78eec0e1fef
dev-Pedrod <pedroolivxira@gmail.com>
2022-03-12 16:25:37 -0300

    theme config

---
+++ myportfy-frontend/src/styles/theme.js
@@ -0,0 +1,24 @@
+export const theme = {
+  colors: {
+    primaryColor: "#A0F3D0",
+    primaryColor_II: "#5cebae",
+    white: "#FFFFFF",
+    mediumGray: "#DDDDDD",
+  },
+  font: {
+    family: {
+      default: "'Montserrat', sans-serif",
+      secondary: "'Open Sans', sans-serif",
+    },
+    sizes: {
+      xsmall: "0.8rem",
+      small: "1.6rem",
+      medium: "2.4rem",
+      large: "3.2rem",
+      xlarge: "4.0rem",
+      xxlarge: "4.8rem",
+      huge: "5.6rem",
+      xhuge: "6.4rem",
+    },
+  },
+};
