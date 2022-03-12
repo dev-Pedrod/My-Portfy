a30adfd889e44f88ad6c4fc68bae165e23637dbc
dev-Pedrod <pedroolivxira@gmail.com>
2022-03-12 14:59:16 -0300

    configuring fonts

--- myportfy-frontend/src/styles/global-styles.js
+++ myportfy-frontend/src/styles/global-styles.js
@@ -2,10 +2,18 @@ import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyles = createGlobalStyle`
 
+    @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@500&family=Rubik+Mono+One&family=Teko:wght@300&display=swap');
+
+    :root {
+        --primary-font: 'Montserrat', sans-serif;
+        --secondary-font: 'Rubik Mono One', sans-serif;
+    }
+
     * {
         box-sizing: border-box;
         margin: 0;
         padding: 0;
+        font-family: var(--secondary-font);
     }
     
 `
