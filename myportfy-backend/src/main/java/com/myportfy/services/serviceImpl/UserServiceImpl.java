bf771c376290796eed76341a3e037d0ee1d560a2
dev-Pedrod <pedroolivxira@gmail.com>
2022-06-04 13:40:25 -0300

    refactor(uploadPicture): delete redundant code

--- myportfy-backend/src/main/java/com/myportfy/services/serviceImpl/UserServiceImpl.java
+++ myportfy-backend/src/main/java/com/myportfy/services/serviceImpl/UserServiceImpl.java
@@ -168,7 +168,6 @@ public class UserServiceImpl implements IUserService {
     @Override
     public URI uploadProfilePicture(MultipartFile multipartFile) {
         User user = findById(currentUserLoggedIn().getId());
-        isCurrentUserLoggedIn(user.getId());
         URI uri = s3Service.uploadFile(multipartFile);
         user.setProfilePictureURL(uri.toString());
         userRepository.saveAndFlush(user);
