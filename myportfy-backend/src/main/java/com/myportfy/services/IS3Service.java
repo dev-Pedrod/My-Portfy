bc592c7a418581eeb95c71c170b4fc3ad3e75649
João Pedro <pedroolivxira@gmail.com>
2022-06-28 20:28:39 -0300

    feat(S3Service): Delete picture

--- myportfy-backend/src/main/java/com/myportfy/services/IS3Service.java
+++ myportfy-backend/src/main/java/com/myportfy/services/IS3Service.java
@@ -1,11 +1,9 @@
 package com.myportfy.services;
 
-import org.springframework.web.multipart.MultipartFile;
-
 import java.io.InputStream;
 import java.net.URI;
 
 public interface IS3Service {
-    URI uploadFile(MultipartFile multipartFile);
     URI uploadFile(InputStream is, String fileName, String contentType);
+    void deletePicture(String key);
 }
--- myportfy-backend/src/main/java/com/myportfy/services/serviceImpl/S3ServiceImpl.java
+++ myportfy-backend/src/main/java/com/myportfy/services/serviceImpl/S3ServiceImpl.java
@@ -1,6 +1,7 @@
 package com.myportfy.services.serviceImpl;
 
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.myportfy.services.IS3Service;
 import com.myportfy.services.exceptions.FileException;
@@ -9,9 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -26,19 +25,6 @@ public class S3ServiceImpl implements IS3Service {
     private String bucketName;
 
     @Override
-    public URI uploadFile(MultipartFile multipartFile) {
-        try {
-            return uploadFile(
-                    multipartFile.getInputStream(),
-                    multipartFile.getOriginalFilename(),
-                    multipartFile.getContentType());
-        } catch (IOException e) {
-            throw new FileException("Erro de IO: " + e.getMessage());
-        }
-    }
-
-    @Override
-    @Async
     public URI uploadFile(InputStream is, String fileName, String contentType) {
         try {
             ObjectMetadata meta = new ObjectMetadata();
@@ -50,5 +36,13 @@ public class S3ServiceImpl implements IS3Service {
         } catch (URISyntaxException e) {
             throw new FileException("Erro ao converter URL para URI");
         }
+    }
+
+    @Override
+    @Async
+    public void deletePicture(String key) {
+        log.info("Deletando imagem: {}", key);
+        DeleteObjectRequest request = new DeleteObjectRequest(bucketName, key);
+        amazonS3.deleteObject(request);
     }
 }
