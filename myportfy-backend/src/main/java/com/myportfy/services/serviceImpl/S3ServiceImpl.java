71164469d84b8a1d18e5ffbf2fde6e0e0cbca519
João Pedro <pedroolivxira@gmail.com>
2022-07-19 06:35:44 -0300

    feat(Logs): Add logs to s3 service

--- myportfy-backend/src/main/java/com/myportfy/services/serviceImpl/S3ServiceImpl.java
+++ myportfy-backend/src/main/java/com/myportfy/services/serviceImpl/S3ServiceImpl.java
@@ -19,6 +19,8 @@ import java.net.URISyntaxException;
 @Slf4j
 public class S3ServiceImpl implements IS3Service {
 
+    private final String FILE_EXCEPTION_MESSAGE = "Erro ao converter URL para URI";
+
     @Autowired
     private AmazonS3 amazonS3;
     @Value("${s3.bucket}")
@@ -29,19 +31,19 @@ public class S3ServiceImpl implements IS3Service {
         try {
             ObjectMetadata meta = new ObjectMetadata();
             meta.setContentType(contentType);
-            log.info("Iniciando upload");
+            log.info("Upload started for file: {}", fileName);
             amazonS3.putObject(bucketName, fileName, is, meta);
-            log.info("Upload finalizado, objeto: {}", fileName);
+            log.info("Upload completed for object: {}", fileName);
             return amazonS3.getUrl(bucketName, fileName).toURI();
         } catch (URISyntaxException e) {
-            throw new FileException("Erro ao converter URL para URI");
+            throw new FileException(FILE_EXCEPTION_MESSAGE);
         }
     }
 
     @Override
     @Async
     public synchronized void deletePicture(String key) {
-        log.info("Deletando imagem: {}", key);
+        log.info("Deleting image: {}", key);
         DeleteObjectRequest request = new DeleteObjectRequest(bucketName, key);
         amazonS3.deleteObject(request);
     }
