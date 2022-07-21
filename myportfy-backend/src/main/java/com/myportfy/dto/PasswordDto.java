4a7933de5c5766d79755b103bbd08d1357afb37e
João Pedro <pedroolivxira@gmail.com>
2022-07-21 08:31:34 -0300

    refactor(Passowrd): Change name passwordUpdate to password

--- myportfy-backend/src/main/java/com/myportfy/dto/PasswordDto.java
+++ myportfy-backend/src/main/java/com/myportfy/dto/PasswordDto.java
@@ -1,6 +1,6 @@
 package com.myportfy.dto;
 
-import com.myportfy.utils.validators.password.PasswordUpdate;
+import com.myportfy.utils.validators.password.Password;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
@@ -9,14 +9,14 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Getter
-@PasswordUpdate
+@Password
 @NoArgsConstructor
-public class PasswordUpdateDto {
-    @NotNull(message = "A senha  não pode ser vazio.")
+public class PasswordDto {
+    @NotNull(message = "A senha não pode ser vazio.")
     @NotBlank(message = "A senha não pode ser em branco.")
     @Length(min = 8, max = 32, message = "O tamanho deve ser entre 8 e 32 caracteres.")
     private String password;
-    @NotNull(message = "A confirmação  não pode ser vazio.")
+    @NotNull(message = "A confirmação não pode ser vazio.")
     @NotBlank(message = "A confirmação não pode ser em branco.")
     @Length(min = 8, max = 32, message = "O tamanho deve ser entre 8 e 32 caracteres.")
     private String confirmPassword;
--- myportfy-backend/src/main/java/com/myportfy/utils/validators/password/PasswordValidator.java
+++ myportfy-backend/src/main/java/com/myportfy/utils/validators/password/PasswordValidator.java
@@ -1,6 +1,6 @@
 package com.myportfy.utils.validators.password;
 
-import com.myportfy.dto.PasswordUpdateDto;
+import com.myportfy.dto.PasswordDto;
 import com.myportfy.services.exceptions.InvalidPasswordException;
 
 import javax.validation.ConstraintValidator;
@@ -8,7 +8,7 @@ import javax.validation.ConstraintValidatorContext;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class PasswordValidator implements ConstraintValidator<PasswordUpdate, PasswordUpdateDto> {
+public class PasswordValidator implements ConstraintValidator<Password, PasswordDto> {
 
     public static Boolean validatePassword(String password) {
         Pattern pattern = Pattern.compile("^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=\\S+$).{8,32}$");
@@ -16,7 +16,7 @@ public class PasswordValidator implements ConstraintValidator<PasswordUpdate, Pa
         return matcher.find();
     }
 
-    public static void validatePasswordUpdate(PasswordUpdateDto passwordUpdate){
+    public static void validatePasswordUpdate(PasswordDto passwordUpdate){
         if(!validatePassword(passwordUpdate.getPassword())){
             throw new InvalidPasswordException("A senha deve conter letras maiúsculas, minúsculas e números.");
         }
@@ -26,12 +26,12 @@ public class PasswordValidator implements ConstraintValidator<PasswordUpdate, Pa
     }
 
     @Override
-    public void initialize(PasswordUpdate constraintAnnotation) {
+    public void initialize(Password constraintAnnotation) {
         ConstraintValidator.super.initialize(constraintAnnotation);
     }
 
     @Override
-    public boolean isValid(PasswordUpdateDto value, ConstraintValidatorContext context) {
+    public boolean isValid(PasswordDto value, ConstraintValidatorContext context) {
         validatePasswordUpdate(value);
         return true;
     }
