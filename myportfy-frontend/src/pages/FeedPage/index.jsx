453290f4c864152b55159b052c110fc4b54dcdae
João Pedro <pedroolivxira@gmail.com>
2022-07-29 09:22:16 -0300

    feat(Feed): Add post input component and styles

--- myportfy-frontend/src/pages/FeedPage/index.jsx
+++ myportfy-frontend/src/pages/FeedPage/index.jsx
@@ -1,10 +1,14 @@
 import React from "react";
-import { GridThreeColumn } from "../../components/GridThreeColumn";
+
+// styles
+import { Line } from "./styles";
 
 // components
+import { GridThreeColumn } from "../../components/GridThreeColumn";
 import { Navbar } from "../../components/Navbar";
 import { NavbarBottom } from "../../components/NavbarBottom";
 import { Post } from "../../components/PostComponent";
+import { PostInputComponent } from "../../components/PostInputComponent";
 import { Sidebar } from "../../components/Sidebar";
 
 export const FeedPage = ({ toggle, isOpen }) => {
@@ -14,7 +18,15 @@ export const FeedPage = ({ toggle, isOpen }) => {
     <>
       <Navbar toggle={toggle} isOpen={isOpen} />
       <Sidebar isOpen={isOpen} toggle={toggle} />
-      <GridThreeColumn middleComponent={<Post />} />
+      <GridThreeColumn
+        middleComponent={
+          <>
+            <PostInputComponent />
+            <Line/>
+            <Post />
+          </>
+        }
+      />
       <NavbarBottom />
     </>
   );
