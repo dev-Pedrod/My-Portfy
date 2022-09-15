1bca4fc9194ce49be6de50684e18875b7091123e
João Pedro <pedroolivxira@gmail.com>
2022-09-15 09:39:43 -0300

    feat(Template): Template service and repository

---
+++ myportfy-backend/src/main/java/com/myportfy/repositories/TemplateRepository.java
@@ -0,0 +1,9 @@
+package com.myportfy.repositories;
+
+import com.myportfy.domain.Template;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TemplateRepository extends JpaRepository<Template, Long> {
+}
