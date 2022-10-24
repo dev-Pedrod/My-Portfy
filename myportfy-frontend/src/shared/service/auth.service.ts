104bd80d90b6aa92abd8972e6b749d6b5274675b
João Pedro <pedroolivxira@gmail.com>
2022-10-24 08:18:10 -0300

    feat(Services): auth service and forgot Password method

---
+++ myportfy-frontend/src/shared/service/auth.service.ts
@@ -0,0 +1,13 @@
+import {api} from "../../api/api";
+import {ServiceType} from "../types/serviceType";
+import {AxiosError} from "axios";
+
+export async function forgotPassword({onError, onSuccess, data}: ServiceType<string>) {
+  await api
+    .post(`/auth/forgot-password?email=${data}`).then((response) => {
+      onSuccess?.(response);
+    }).catch((err: AxiosError) => {
+      console.error(err);
+      onError?.(err);
+    })
+}
