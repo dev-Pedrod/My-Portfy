893186f1b00dea7a7b94368ee2e239f3e5b78627
João Pedro <pedroolivxira@gmail.com>
2022-10-28 09:11:28 -0300

    feat(Service): Consume post api

--- myportfy-frontend/src/types/post.ts
+++ myportfy-frontend/src/types/post.ts
@@ -2,14 +2,24 @@ import {User} from "./user";
 import {Category} from "./category";
 
 export type Post = {
-  id: number;
-  createdAt: string;
-  disabledAt: string;
-  updatedAt: string;
+  id?: number;
+  createdAt?: string;
+  disabledAt?: string;
+  updatedAt?: string;
   title: string;
   content: string;
   description: string;
-  author: User;
-  imageURL: string;
-  categories: Array<Category>;
+  author?: User;
+  imageURL?: string;
+  categories?: Array<Category>;
+}
+
+export type uploadImageType = {
+  id: number;
+  image: string | Blob
+}
+
+export type updatePostType = {
+  id: number;
+  post: Post
 }
