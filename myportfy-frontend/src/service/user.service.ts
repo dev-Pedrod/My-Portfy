78e92aa46da4c6fd54b0bc0968dc3ad6c01040dd
João Pedro <pedroolivxira@gmail.com>
2022-11-07 06:58:07 -0300

    feat(service): reset-passowrd service

--- myportfy-frontend/src/service/user.service.ts
+++ myportfy-frontend/src/service/user.service.ts
@@ -1,7 +1,7 @@
 import {ServiceType} from "../types/serviceType";
 import {api} from "../api/api";
 import {AxiosError} from "axios";
-import {User} from "../types/user";
+import {Password, User} from "../types/user";
 
 export async function getUserById({onError, onSuccess, data}: ServiceType<string>) {
   return await api
@@ -17,6 +17,17 @@ export async function getUserById({onError, onSuccess, data}: ServiceType<string
 export async function createUser({onError, onSuccess, data}: ServiceType<User>) {
   return await api
     .post("/users", data)
+    .then((response) => {
+      return onSuccess ? onSuccess(response) : response
+    }).catch((err: AxiosError) => {
+      console.error(err);
+      return onError ? onError(err) : err
+    })
+}
+
+export async function resetPassword(token: string, {onError, onSuccess, data}: ServiceType<Password>) {
+  return await api
+    .put(`/users/reset-password?token=${token}`, data)
     .then((response) => {
       return onSuccess ? onSuccess(response) : response
     }).catch((err: AxiosError) => {
