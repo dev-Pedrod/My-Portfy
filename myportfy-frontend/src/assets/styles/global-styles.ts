737443ebe17255eb345d6899a777a11c37f5e055
João Pedro <pedroolivxira@gmail.com>
2022-12-04 11:19:25 -0300

    Styled scroll bar

--- myportfy-frontend/src/assets/styles/global-styles.ts
+++ myportfy-frontend/src/assets/styles/global-styles.ts
@@ -1,20 +1,39 @@
-import {createGlobalStyle} from "styled-components";
+import {createGlobalStyle, css} from "styled-components";
 
 export const GlobalStyles = createGlobalStyle`
-  * {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-    font-size: ${({theme}) => theme.font.sizes.small};
-    font-family: ${({theme}) => theme.font.family.default};
-    scroll-behavior: smooth;
-  }
+  ${({theme}) => css`
+    * {
+      box-sizing: border-box;
+      margin: 0;
+      padding: 0;
+      font-size: ${({theme}) => theme.font.sizes.small};
+      font-family: ${({theme}) => theme.font.family.default};
+      scroll-behavior: smooth;
 
-  html {
-    font-size: 62.5%;
-  }
+      ::-webkit-scrollbar {
+        width: 1rem;
+      }
 
-  h1, h2, h3, h4, h5, h6 {
-    font-family: ${({theme}) => theme.font.family.secondary};
-  }
+      ::-webkit-scrollbar-track {
+        background: ${theme.colors.ligthGray};
+      }
+
+      ::-webkit-scrollbar-thumb {
+        background: ${theme.colors.mediumGray};
+      }
+
+      ::-webkit-scrollbar-thumb:hover {
+        background: ${theme.colors.Gray};
+      }
+    }
+
+    html {
+      font-size: 62.5%;
+    }
+
+    h1, h2, h3, h4, h5, h6 {
+      font-family: ${({theme}) => theme.font.family.secondary};
+    }
+
+  `}
 `;
